1bb82caf825eb01a9f10d8f5f5516599e59b7751
Daniela Ruiz <daruiz1218@gmail.com>
2023-08-24 08:13:03 -0500

    feat: admin role

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'dashboard']);
+        Permission::create(['name' => 'users.index']);
+        Permission::create(['name' => 'users.show']);
+        Permission::create(['name' => 'users.edit']);
+        Permission::create(['name' => 'users.update']);
+        Permission::create(['name' => 'users.destroy']);
+
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleAdmin->givePermissionTo(Permission::all());
+    }
+}
